9e78fce332a488abad68af6c1fc29a4dfd04b821
xdirdeto <xiaodong.yu@irdeto.com>
2012-06-17 21:16:22 +0200

    Initial Commit

---
+++ xcodetest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  xcodetest
+//
+//  Created by Xiaodong Yu on 17/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
